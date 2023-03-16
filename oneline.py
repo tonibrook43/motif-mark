af3f740f8d5b4ef76bb4b1ae804e947f69fc0756
tonibrook43 <93627688+tonibrook43@users.noreply.github.com>
2023-03-16 12:02:21 -0700

    Update oneline.py

--- oneline.py
+++ oneline.py
@@ -14,22 +14,16 @@ import argparse
 def get_args():
     parser = argparse.ArgumentParser(description="A program to convert multi-line fasta into one line fasta")
     parser.add_argument("-f", "--file", help="designates absolute file path to fasta file", required=True)
-    parser.add_argument("-o", "--outfile", help="designates absolute file path to the output file", required=False)    
     return parser.parse_args()
 args=get_args()
 
-# to run script: ./oneline.py -f /Users/tonibrooks/bioinfo/OOP/Figure_1.fasta
-
-
 def oneline_fasta():
     '''Makes FASTA sequences on one line.'''
-    # make dict with headers as keys and sequences as values
     sequence_dictionary = {}
     header_line = ""
     with open(args.file, 'r') as fasta:
         for line in fasta:
             line = line.strip('\n')
-            # only get header lines
             if line[0] == '>':
                 header_line = line
             else:
@@ -39,8 +33,8 @@ def oneline_fasta():
                     sequence_dictionary[header_line] += line
     # write out to file
     oneline_fasta = open('oneline_fasta.fa', 'w')
-    for keys,vals in sequence_dictionary.items():
-        oneline_fasta.write(str(keys) + '\n' + str(vals) + '\n')
+    for keys, values in sequence_dictionary.items():
+        oneline_fasta.write(str(keys) + '\n' + str(values) + '\n')
 
     oneline_fasta.close()
 
